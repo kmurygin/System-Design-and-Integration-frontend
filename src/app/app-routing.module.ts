6b14d5f65123dac040a30477bba323b1e82647eb
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-23 14:42:12 +0100

    PIS23Z-32-Used routing to display userlist when chosen in navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserlistComponent } from './userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'userlist', component: UserlistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
