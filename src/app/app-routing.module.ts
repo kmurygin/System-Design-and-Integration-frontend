b92abb6f5dbad5d4ff8d0cf69e5e16632c52b9c0
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-27 18:06:57 +0100

    PIS23Z-32-Created route for userlist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserlistComponent } from './userlist/userlist.component';
+import {LandingpageComponent} from "./landingpage/landingpage.component";
 
 const routes: Routes = [
-  { path: 'userlist', component: UserlistComponent }
+  { path: 'userlist', component: UserlistComponent },
+  { path: '', component: LandingpageComponent},
 ];
 
 @NgModule({
